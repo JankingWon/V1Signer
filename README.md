3eb2b79c8f40752c1853f8ea765736e9dd250adf
Janking <37773224+JankingWon@users.noreply.github.com>
2020-04-15 22:19:37 +0800

    feat 合并apksigner和jarsigner

--- README.md
+++ README.md
@@ -1,30 +1,61 @@
 ## 说明
 
-本项目使用的是v1签名，可适用所有Android版本。
+本项目包含两个`Module`
 
-本项目的关于签名的源码全部来源于 https://github.com/frohoff/jdk8u-jdk ，用于给`Android Apk`签名
+* `JarSigner`
+* `ApkSigner`
 
-之所以要有这个项目，是因为`Android Studio`中是用不了`jdk`中的库，所以需要手动把以来挑出来
-
-1. 如果是使用`java`开发，只需要复制 `com.sun.jarsigner` 和 `sun.security.tools.jarsigner` 两个`package`到你的项目中即可
-2. 如果是使用`android`开发，则需要手动导入本项目中的所有源代码
+使用的均是V1签名，可适用所有Android版本。
 
 ## 效果
 
-![image-20200317204332430](README.assets/image-20200317204332430.png)
+<img src="README.assets/image-20200415221027516.png" width="369"></img>
 
-## 使用
+## JarSigner
 
-1. 直接使用打包好的`jar`包([链接](https://github.com/JankingWon/JarSigner/releases))，在命令行中输入
+### 说明
+
+* 本`Module`的关于签名的源码全部来源于 https://github.com/frohoff/jdk8u-jdk ，用于给`Android Apk`签
+  1. 如果是使用`java`开发，只需要复制 `com.sun.jarsigner` 和 `sun.security.tools.jarsigner` 两个`package`到你的项目中即可
+  2. 如果是使用`android`开发，则需要手动导入本项目中的所有源代码
+
+### 使用
+
+1. 直接使用打包好的`jarsigner.jar`包([链接](https://github.com/JankingWon/V1Signer/releases))，在命令行中输入
 
    ```bash
    //linux
    $jarsigner [-verbose] -keystore "~/Desktop/test.jks"  -storepass 123456  -keyPass 123456 -signedjar "~/Desktop/test.apk" "~/Desktop/unsigned.apk" test
    //windows
-   $java -jar .\jarsigner.jar -verbose -keystore "C:\\Users\\janki\\Desktop\\test.jks"  -storepass 123456  -keyPass 123456 -signedjar "C:\\Users\\janki\\Desktop\\test.apk" "C:\\Users\\janki\\Desktop\\unsigned.apk" test
+   $java -jar .\jarsigner.jar [-verbose] -keystore "C:\\test.jks"  -storepass 123456  -keyPass 123456 -signedjar "C:\\test.apk" "C:\\unsigned.apk" test
    ```
 
 2. 使用源码，调用`sun.security.tools.jarsigner.Main#main(String[] args)`方法，参数同上
 
-## 注意
+### 注意
+
 如果是在`Android`设备上适用，要使用`BKS-V1`类型的秘钥，一般用`AndroidStudio`生成的`jks`秘钥是`JKS`类型，可以使用 [KeyStore Explorer](https://keystore-explorer.org/) 查看和转换签名类型
+
+## ApkSigner
+
+### 说明
+
+* 本`Module`的关于签名的源码来源于`Android`源码和`jdk1.8.0_241`，使用`IntelliJ Idea`反编译获得，用于给`Android Apk`签名
+  1. 如果是使用`java`开发，只需要复制 `SignApk.java`([链接](https://github.com/JankingWon/V1Signer/blob/master/apksigner/src/main/java/com/android/signapk/SignApk.java))文件到你的项目中即可
+  2. 如果是使用`android`开发，则需要手动导入本项目中的所有源代码
+
+
+### 使用
+
+1. 直接使用打包好的`apksigner.jar`包([链接](https://github.com/JankingWon/V1Signer/releases))，在命令行中输入
+
+   ```bash
+   //linux
+   $signapk [-w] publickey.x509[.pem] privatekey.pk8 input.jar output.jar
+   //windows
+   $java -jar apksigner.jar [-w] publickey.x509[.pem] privatekey.pk8 input.jar output.jar
+   ```
+
+2. 使用源码，调用`SignApk#Main(String[] args)`方法，参数同上
+
+
--- app/src/main/java/cn/janking/jarsigner/MainActivity.kt
+++ app/src/main/java/cn/janking/jarsigner/MainActivity.kt
@@ -1,155 +1,113 @@
 package cn.janking.jarsigner
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri.fromParts
 import android.os.Build
 import android.os.Bundle
-import android.os.Environment
 import android.provider.Settings
 import android.text.method.ScrollingMovementMethod
 import android.view.View
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import com.android.signapk.SignApk
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.File
 import java.io.IOException
 
 
 class MainActivity : AppCompatActivity() {
-    private val CODE_OPEN_UNSIGNED_APK = 1
-    private val CODE_REQUEST_STORAGE_PERMISSION = 2
-    private val GROUP_STORAGE = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE)
-    private val KEY = "cert.jks"
+    /**
+     * 选择未签名文件的request code
+     */
+    private val codePickUnsignedApk = 1
 
+    /**
+     * 请求权限的request code
+     */
+    private val codeRequestStoragePermission = 2
+
+    /**
+     * 读写权限组
+     */
+    private val permissionGroupStorage = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+
+    /**
+     * jarSigner需要的的签名文件
+     */
+    private val keyJarSigner = "cert.jks"
+
+    /**
+     * apkSigner需要的签名文件
+     */
+    private val keyApkSignerPrivate = "platform.pk8"
+    private val keyApkSignerPublic = "platform.x509.pem"
+
+    /**
+     * 用来复制签名的Runnable
+     */
+    private val runnableCopySignFile = Runnable {
+        try {
+            FileUtils.copyFileToFile(assets.open(keyJarSigner), filesDir.absolutePath + File.separator + keyJarSigner)
+            FileUtils.copyFileToFile(assets.open(keyApkSignerPrivate), filesDir.absolutePath + File.separator + keyApkSignerPrivate)
+            FileUtils.copyFileToFile(assets.open(keyApkSignerPublic), filesDir.absolutePath + File.separator + keyApkSignerPublic)
+        } catch (e: IOException) {
+            log("复制签名出错！" + e.message)
+            e.printStackTrace()
+        }
+        if (!hasInit) {
+            log("复制签名完成！")
+        }
+        hasInit = true
+    }
+    /**
+     * 是否把签名复制完成
+     */
     private var hasInit: Boolean = false
+
+    /**
+     * 未签名文件
+     */
     private var unsignedApkFile: File? = null
+
+    /**
+     * 签名后APK保存文件夹
+     */
+    lateinit var outPath: String
+
+    /**
+     * 签名后APK完整路径
+     */
+    lateinit var outFile: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //可以滚动
-        console.run { console.setMovementMethod(ScrollingMovementMethod()) }
-        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.M){
-            try {
-                FileUtils.copyFileToFile(assets.open(KEY), filesDir.absolutePath + File.separator + KEY)
-            } catch (e: IOException) {
-                log("复制签名出错！" + e.message)
-                e.printStackTrace()
-            }
-            if (!hasInit) {
-                log("复制签名完成！")
-            }
-            hasInit = true
+        //设置可以滚动
+        console.run { console.movementMethod = ScrollingMovementMethod() }
+        //6.0以下直接复制签名文件
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            Thread(runnableCopySignFile).start()
         }
+        //优先使用外置存储中本项目的file文件夹，其次是内部file文件夹
+        outPath = getExternalFilesDir("V1Signer")?.absolutePath
+                ?: filesDir.absolutePath + File.separator + "V1Signer"
+        outFile = outPath + File.separator + "out.apk"
     }
 
+    /**
+     * 检查权限
+     */
     override fun onStart() {
         super.onStart()
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            requestPermissions(GROUP_STORAGE, CODE_REQUEST_STORAGE_PERMISSION)
-        }
-
-    }
-
-    /**
-     * 选择签名文件
-     */
-    fun pickUnsignedApkFile(view: View) {
-        val intent = Intent(Intent.ACTION_GET_CONTENT)
-        intent.type = "*/*"
-        intent.addCategory(Intent.CATEGORY_OPENABLE)
-        startActivityForResult(intent, CODE_OPEN_UNSIGNED_APK)
-    }
-
-    /**
-     * 选择文件返回
-     */
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == CODE_OPEN_UNSIGNED_APK) {
-            data?.data?.let {
-                unsignedApkFile = UriUtils.uri2File(application, it)?.apply {
-                    pickUnsignedApkFile.text = absolutePath
-                }
-                log("准备就绪...")
-            }
+            requestPermissions(permissionGroupStorage, codeRequestStoragePermission)
         }
     }
 
-    /**
-     * 执行签名
-     */
-    fun startSign(view: View) {
-        unsignedApkFile?.let {
-            if (!it.exists()) {
-                log("待签名apk不存在！")
-                return
-            } else if (FileUtils.getFileExtension(it) != "apk" &&
-                    FileUtils.getFileExtension(it) != "jar" &&
-                    FileUtils.getFileExtension(it) != "zip") {
-                log("待签名文件必须是(.apk|.jar|.zip)！")
-                return
-            }
-            log("开始签名...")
-            val outPath: String = Environment.getExternalStorageDirectory().absolutePath + File.separator + "JarSigner"
-            val outFile: String = outPath + File.separator + "out.apk"
-            Thread(Runnable {
-                try {
-                    //签名
-                    sun.security.tools.jarsigner.Main.main(
-                            arrayOf(
-                                    "-verbose",
-                                    "-keystore", filesDir.absolutePath + File.separator + KEY,
-                                    "-storepass", "123456",
-                                    "-keyPass", "123456",
-                                    "-signedjar",
-                                    FileUtils.getExistFile(outFile).absolutePath,
-                                    it.absolutePath,
-                                    "test"
-                            )
-                    )
-                } catch (e: java.lang.Exception) {
-                    log("签名出错...(${e.message})")
-                    e.printStackTrace()
-                    return@Runnable
-                }
-                log("签名完成！($outFile)")
-            }).start()
-
-        } ?: log("请选择待签名apk文件！")
-
-    }
-
-    /**
-     * 打开文件夹
-     */
-//    private fun openAssignFolder(path: String) {
-//        val file = File(path)
-//        if (!file.exists()) {
-//            return
-//        }
-//        val intent = Intent(Intent.ACTION_GET_CONTENT)
-//        intent.addCategory(Intent.CATEGORY_OPENABLE)
-//        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-//        intent.setDataAndType(UriUtils.file2Uri(application, file), "file/*")
-//        try {
-//            startActivity(intent)
-//        } catch (e: ActivityNotFoundException) {
-//            e.printStackTrace()
-//        }
-//    }
-
-    /**
-     * 输出运行日志
-     */
-    private fun log(msg: String) {
-        runOnUiThread {
-            console.setText("${console.text}\n${msg}")
-        }
-    }
 
     /**
      * 请求权限返回
@@ -175,26 +133,124 @@ class MainActivity : AppCompatActivity() {
                                 }
                             }.show()
                 } else {
-                    requestPermissions(GROUP_STORAGE, CODE_REQUEST_STORAGE_PERMISSION)
+                    requestPermissions(permissionGroupStorage, codeRequestStoragePermission)
                 }
                 return
             }
         }
         //表示已经授权
-        try {
-            FileUtils.copyFileToFile(assets.open(KEY), filesDir.absolutePath + File.separator + KEY)
-        } catch (e: IOException) {
-            log("复制签名出错！" + e.message)
-            e.printStackTrace()
-            return
-        }
-        if (!hasInit) {
-            log("复制签名完成！")
-        }
-        hasInit = true
+        Thread(runnableCopySignFile).start()
     }
 
+    /**
+     * 判断权限是否被允许
+     */
     private fun isGranted(permission: String): Boolean {
         return Build.VERSION.SDK_INT < Build.VERSION_CODES.M || PackageManager.PERMISSION_GRANTED == ContextCompat.checkSelfPermission(applicationContext, permission)
+    }
+
+
+    /**
+     * 在屏幕上输出运行日志
+     */
+    @SuppressLint("SetTextI18n")
+    private fun log(msg: String) {
+        runOnUiThread {
+            console.text = "${console.text}\n${msg}"
+        }
+    }
+
+    /**
+     * 选择签名文件
+     */
+    fun pickUnsignedApkFile(view: View) {
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "*/*"
+        intent.addCategory(Intent.CATEGORY_OPENABLE)
+        startActivityForResult(intent, codePickUnsignedApk)
+    }
+
+    /**
+     * 选择文件返回
+     */
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == codePickUnsignedApk) {
+            data?.data?.let {
+                unsignedApkFile = UriUtils.uri2File(application, it)?.apply {
+                    pickUnsignedApkFile.text = absolutePath
+                    log("成功加载文件(${name})")
+                }
+            }
+        }
+    }
+
+
+    /**
+     * 执行ApkSigner
+     */
+    fun startApkSigner(view: View) {
+        log("[ApkSigner]")
+        performSign {
+            //签名
+            SignApk.main(
+                    arrayOf(
+                            filesDir.absolutePath + File.separator + keyApkSignerPublic,
+                            filesDir.absolutePath + File.separator + keyApkSignerPrivate,
+                            it,
+                            FileUtils.getExistFile(outFile).absolutePath
+                    )
+            )
+        }
+    }
+
+    /**
+     * 执行JarSigner
+     */
+    fun startJarSigner(view: View) {
+        log("[JarSigner]")
+        performSign {
+            //签名
+            sun.security.tools.jarsigner.Main.main(
+                    arrayOf(
+                            "-verbose",
+                            "-keystore", filesDir.absolutePath + File.separator + keyJarSigner,
+                            "-storepass", "123456",
+                            "-keyPass", "123456",
+                            "-signedjar",
+                            FileUtils.getExistFile(outFile).absolutePath,
+                            it,
+                            "test"
+                    )
+            )
+        }
+    }
+
+
+    /**
+     * 执行签名
+     */
+    private fun performSign(signAction: (String)-> Unit){
+        return unsignedApkFile?.let {
+            if (!it.exists()) {
+                log("待签名apk不存在！")
+            } else if (FileUtils.getFileExtension(it) != "apk" &&
+                    FileUtils.getFileExtension(it) != "jar" &&
+                    FileUtils.getFileExtension(it) != "zip") {
+                log("待签名文件必须是(.apk|.jar|.zip)！")
+            }
+            log("开始签名...")
+            Thread(Runnable {
+                try {
+                    //签名
+                    signAction(it.absolutePath)
+                } catch (e: java.lang.Exception) {
+                    log("签名出错...(${e.message})")
+                    e.printStackTrace()
+                    return@Runnable
+                }
+                log("签名完成！($outFile)")
+            }).start()
+        } ?: log("请选择待签名apk文件！")
     }
 }
